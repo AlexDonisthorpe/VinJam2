0408089e86616955d5bf7ccaaba732ad54ab915e
Alex Donisthorpe <alex.donisthorpe+doneecans@gmail.com>
2021-09-13 19:20:03 +0100

    Implemented some placeholder UI for the level timer & hooked it up to the timer values

--- Assets/Scripts/LevelTimer.cs
+++ Assets/Scripts/LevelTimer.cs
@@ -2,17 +2,22 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Serialization;
+using UnityEngine.UI;
 
 public class LevelTimer : MonoBehaviour
 {
     [SerializeField] private float levelDurationInSeconds = 60f;
     
-    private bool _timer = false;
     [SerializeField] private float levelTimer = 60f;
+    private bool _timer = false;
+
+    private Slider _slider;
     
     // Start is called before the first frame update
     void Start()
     {
+        _slider = GetComponentInChildren<Slider>();
+        _slider.maxValue = levelDurationInSeconds;
         StartTimer();
     }
 
@@ -25,10 +30,12 @@ public class LevelTimer : MonoBehaviour
     // Update is called once per frame
     private void Update()
     {
+        _slider.value = levelTimer;
+        
         if (_timer)
         {
             levelTimer -= Time.deltaTime;
-            if(levelTimer <= levelDurationInSeconds) Debug.Log("Level Complete");
+            if(levelTimer <= 0) Debug.Log("Level Complete");
         }
         
     }

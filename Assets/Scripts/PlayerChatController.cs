1f2f8975d6f5969b08265ccec13df104dbbbf63e
Alex Donisthorpe <alex.donisthorpe+doneecans@gmail.com>
2021-09-13 09:33:08 +0100

    Added the player chat display & tweaked the boss chat display

---
+++ Assets/Scripts/PlayerChatController.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class PlayerChatController : MonoBehaviour
+{
+    private static readonly int StartMessage = Animator.StringToHash("StartMessage");
+
+    private void Start()
+    {
+        GetComponent<Animator>().SetTrigger(StartMessage);
+    }
+
+    public void ShowMessage(string message)
+    {
+        GetComponentInChildren<TextMeshProUGUI>().text = message;
+        GetComponent<Animator>().SetTrigger(StartMessage);
+    }
+}
